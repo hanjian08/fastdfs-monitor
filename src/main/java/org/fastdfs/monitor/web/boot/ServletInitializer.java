1a6fbf3989b9e3e963665bb846843f63b3eb0812
583479389 <583479389@qq.com>
2016-09-15 22:19:47 +0800

    fastdfs monitor应用初始化（spring boot，gradle）

---
+++ src/main/java/org/fastdfs/monitor/web/boot/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.fastdfs.monitor.web.boot;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BootApplication.class);
+	}
+
+}
